9b43a913604ecdd601e1e35afd0fabfd0e4d1485
douyouzhe <douyouzhe@gmail.com>
2018-12-15 23:34:32 -0800

    test

--- backend/src/main/java/com/theawesomecompany/projectawesome/ProjectAwesomeApplication.java
+++ backend/src/main/java/com/theawesomecompany/projectawesome/ProjectAwesomeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectAwesomeApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectAwesomeApplication.class, args);
 	}

2a4c835d67bf5b168e66f889a55885a523cdd323
douyouzhe <douyouzhe@gmail.com>
2018-12-15 22:49:24 -0800

    init

---
+++ backend/src/main/java/com/theawesomecompany/projectawesome/ProjectAwesomeApplication.java
@@ -0,0 +1,14 @@
+package com.theawesomecompany.projectawesome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAwesomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAwesomeApplication.class, args);
+	}
+
+}
+

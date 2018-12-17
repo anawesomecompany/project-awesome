2d449c3ef8236a7bf2106ccd6d4763e77a29e8be
douyouzhe <douyouzhe@gmail.com>
2018-12-16 17:50:25 -0800

    implemented service+dao+controller

---
+++ backend/src/main/java/com/theawesomecompany/projectawesome/Dao/impl/TaskDaoImpl.java
@@ -0,0 +1,40 @@
+package com.theawesomecompany.projectawesome.Dao.impl;
+
+import com.theawesomecompany.projectawesome.Dao.TaskDao;
+import com.theawesomecompany.projectawesome.model.Task;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public class TaskDaoImpl implements TaskDao{
+
+
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sf){
+        this.sessionFactory = sf;
+    }
+
+    @Override
+    public void save(Task task) {
+        this.sessionFactory.getCurrentSession().persist(task);
+    }
+
+    @Override
+    public List<Task> loadAll() {
+        Session session = this.sessionFactory.getCurrentSession();
+        List<Task> allTasks = session.createQuery("from TASK_tb").list();
+        return allTasks;
+    }
+
+    @Override
+    public Task findById(Long userId) {
+        Session session = this.sessionFactory.getCurrentSession();
+        Task task = (Task) session.load(Task.class, userId);
+        return task;
+    }
+}
